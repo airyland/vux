01dd2961507b2df4d20d383020d8ce51e8ae197f
airyland <airyland@qq.com>
2016-02-23 07:09:35 +0800

    textarea: remove title

--- src/components/Textarea.vue
+++ src/components/Textarea.vue
@@ -1,15 +1,10 @@
 <template>
-		<div>
-		 	<group-title>{{title}}</group-title>
-	    <div class="weui_cells weui_cells_form">
-	        <div class="weui_cell">
-	            <div class="weui_cell_bd weui_cell_primary">
-	                <textarea class="weui_textarea" placeholder="{{placeholder}}" rows="3" v-model="value"></textarea>
-	                <div class="weui_textarea_counter" v-show="show_counter && max"><span>{{count}}</span>/{{max}}</div>
-	            </div>
-	        </div>
-	    </div>
-    </div>
+  <div class="weui_cell">
+      <div class="weui_cell_bd weui_cell_primary">
+          <textarea class="weui_textarea" placeholder="{{placeholder}}" rows="3" v-model="value"></textarea>
+          <div class="weui_textarea_counter" v-show="showCounter && max"><span>{{count}}</span>/{{max}}</div>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -21,11 +16,7 @@ export default {
     GroupTitle
   },
   props: {
-    title: {
-      type: String,
-      required: true
-    },
-    show_counter: {
+    showCounter: {
       type: Boolean,
       default: true
     },
@@ -34,7 +25,8 @@ export default {
     },
     value: {
       type: String,
-      default: ''
+      default: '',
+      twoWay: true
     },
     placeholder: {
       type: String,
--- src/demos/Textarea.vue
+++ src/demos/Textarea.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-    <textarea title="textarea" :max=200 placeholder="请填写详细信息"></textarea>
+    <group title="textarea">
+      <textarea :max=200 placeholder="请填写详细信息"></textarea>
+    </group>
+    <group title="和input一起使用">
+      <x-input placeholder="标题"></x-input>
+      <textarea :max=200 placeholder="描述"></textarea>
+    </group>
+    <group title="不显示计数器">
+      <textarea :max=200 placeholder="请填写详细信息" :show-counter=false></textarea>
+    </group>
   </div>
 </template>
 
 <script>
-  import { Textarea } from '../components/'
+  import { Textarea, Group, Input as XInput } from '../components/'
   export default {
     components: {
-      Textarea
+      Textarea,
+      Group,
+      XInput
     }
   }
 </script>
