0a87f595b8b2af5fb4dc2a3b10c624b4610789f5
airyland <airyland@qq.com>
2016-03-09 17:09:46 +0800

    Datetime: fix wrong current time fix #23

--- src/components/datetime/format.js
+++ src/components/datetime/format.js
@@ -1,83 +1,33 @@
-module.exports = function formatDate(date, format, utc) {
-  format = format.toLowerCase().replace('ii','mm');
-  var MMMM = ["\x00", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  var MMM = ["\x01", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  var dddd = ["\x02", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  var ddd = ["\x03", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  function ii(i, len) {
-      var s = i + "";
-      len = len || 2;
-      while (s.length < len) s = "0" + s;
-      return s;
+module.exports = function(date, fmt) {
+  var o = {
+    "m+": date.getMonth() + 1,
+    "d+": date.getDate(),
+    "H+": date.getHours() % 12 == 0 ? 12 : date.getHours() % 12,
+    "h+": date.getHours(),
+    "i+": date.getMinutes(),
+    "s+": date.getSeconds(),
+    "q+": Math.floor((date.getMonth() + 3) / 3),
+    "S": date.getMilliseconds()
+  };
+  var week = {
+    "0": "/u65e5",
+    "1": "/u4e00",
+    "2": "/u4e8c",
+    "3": "/u4e09",
+    "4": "/u56db",
+    "5": "/u4e94",
+    "6": "/u516d"
+  };
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
   }
-
-  var y = utc ? date.getUTCFullYear() : date.getFullYear();
-  format = format.replace(/(^|[^\\])yyyy+/g, "$1" + y);
-  format = format.replace(/(^|[^\\])yy/g, "$1" + y.toString().substr(2, 2));
-  format = format.replace(/(^|[^\\])y/g, "$1" + y);
-
-  var M = (utc ? date.getUTCMonth() : date.getMonth()) + 1;
-  format = format.replace(/(^|[^\\])MMMM+/g, "$1" + MMMM[0]);
-  format = format.replace(/(^|[^\\])MMM/g, "$1" + MMM[0]);
-  format = format.replace(/(^|[^\\])MM/g, "$1" + ii(M));
-  format = format.replace(/(^|[^\\])M/g, "$1" + M);
-
-  var d = utc ? date.getUTCDate() : date.getDate();
-  format = format.replace(/(^|[^\\])dddd+/g, "$1" + dddd[0]);
-  format = format.replace(/(^|[^\\])ddd/g, "$1" + ddd[0]);
-  format = format.replace(/(^|[^\\])dd/g, "$1" + ii(d));
-  format = format.replace(/(^|[^\\])d/g, "$1" + d);
-
-  var H = utc ? date.getUTCHours() : date.getHours();
-  format = format.replace(/(^|[^\\])HH+/g, "$1" + ii(H));
-  format = format.replace(/(^|[^\\])H/g, "$1" + H);
-
-  var h = H > 12 ? H - 12 : H == 0 ? 12 : H;
-  format = format.replace(/(^|[^\\])hh+/g, "$1" + ii(h));
-  format = format.replace(/(^|[^\\])h/g, "$1" + h);
-
-  var m = utc ? date.getUTCMinutes() : date.getMinutes();
-  format = format.replace(/(^|[^\\])mm+/g, "$1" + ii(m));
-  format = format.replace(/(^|[^\\])m/g, "$1" + m);
-
-  var s = utc ? date.getUTCSeconds() : date.getSeconds();
-  format = format.replace(/(^|[^\\])ss+/g, "$1" + ii(s));
-  format = format.replace(/(^|[^\\])s/g, "$1" + s);
-
-  var f = utc ? date.getUTCMilliseconds() : date.getMilliseconds();
-  format = format.replace(/(^|[^\\])fff+/g, "$1" + ii(f, 3));
-  f = Math.round(f / 10);
-  format = format.replace(/(^|[^\\])ff/g, "$1" + ii(f));
-  f = Math.round(f / 10);
-  format = format.replace(/(^|[^\\])f/g, "$1" + f);
-
-  var T = H < 12 ? "AM" : "PM";
-  format = format.replace(/(^|[^\\])TT+/g, "$1" + T);
-  format = format.replace(/(^|[^\\])T/g, "$1" + T.charAt(0));
-
-  var t = T.toLowerCase();
-  format = format.replace(/(^|[^\\])tt+/g, "$1" + t);
-  format = format.replace(/(^|[^\\])t/g, "$1" + t.charAt(0));
-
-  var tz = -date.getTimezoneOffset();
-  var K = utc || !tz ? "Z" : tz > 0 ? "+" : "-";
-  if (!utc) {
-      tz = Math.abs(tz);
-      var tzHrs = Math.floor(tz / 60);
-      var tzMin = tz % 60;
-      K += ii(tzHrs) + ":" + ii(tzMin);
+  if (/(E+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, ((RegExp.$1.length > 1) ? (RegExp.$1.length > 2 ? "/u661f/u671f" : "/u5468") : "") + week[date.getDay() + ""]);
   }
-  format = format.replace(/(^|[^\\])K/g, "$1" + K);
-
-  var day = (utc ? date.getUTCDay() : date.getDay()) + 1;
-  format = format.replace(new RegExp(dddd[0], "g"), dddd[day]);
-  format = format.replace(new RegExp(ddd[0], "g"), ddd[day]);
-
-  format = format.replace(new RegExp(MMMM[0], "g"), MMMM[M]);
-  format = format.replace(new RegExp(MMM[0], "g"), MMM[M]);
-
-  format = format.replace(/\\(.)/g, "$1");
-
-  return format;
-};
+  for (var k in o) {
+    if (new RegExp("(" + k + ")").test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    }
+  }
+  return fmt;
+}
