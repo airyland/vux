49bd5f1e4cbbc68c3c1be21a6027618245592eb6
airyland <airyland@qq.com>
2016-03-17 11:38:24 +0800

    respect the eslint

--- build/webpack.npm.conf.js
+++ build/webpack.npm.conf.js
@@ -1,6 +1,5 @@
 var webpack = require('webpack')
 var config = require('./webpack.npm.base.conf')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 // naming output files with hashes for better caching.
 // dist/index.html will be auto-generated with correct URLs.
@@ -12,13 +11,6 @@ config.output.chunkFilename = '[id].[chunkhash].js'
 var SOURCE_MAP = false
 
 config.devtool = SOURCE_MAP ? 'source-map' : false
-
-// generate loader string to be used with extract text plugin
-function generateExtractLoaders (loaders) {
-  return loaders.map(function (loader) {
-    return loader + '-loader' + (SOURCE_MAP ? '?sourceMap' : '')
-  }).join('!')
-}
 
 config.vue.loaders = {
   js: 'babel!eslint'
