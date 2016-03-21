4a6c0de338f9b3caa1dff1b650dc8c32142f821e
airyland <airyland@qq.com>
2016-03-21 15:14:12 +0800

    Flexbox: Support vertical orient

--- src/components/flexbox/index.vue
+++ src/components/flexbox/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="vux-flexbox">
+  <div class="vux-flexbox" :class="{'flex-col': orient == 'vertical', 'flex-row': orient == 'horizontal'}">
     <slot></slot>
   </div>
 </template>
@@ -30,6 +30,10 @@ export default {
     marginLeft: {
       type: Number,
       default: 8
+    },
+    orient: {
+      type: String,
+      default: 'horizontal'
     }
   }
 }
@@ -57,7 +61,31 @@ export default {
   min-width: 20px;
 }
 
+.vux-flexbox-item > .vux-flexbox {
+  width: 100%;
+  height: 100%;
+}
+
 .vux-flexbox .vux-flexbox-item:first-child{
   margin-left: 0!important;
+}
+
+.flex-col {
+  box-orient: vertical;
+  flex-direction: column;
+}
+
+.flex-col > .vux-flexbox-item {
+  width: 100%;
+}
+
+.flex-row {
+  box-direction: row;
+  box-orient: horizontal;
+  flex-direction: row;
+}
+
+.flex-row > .vux-flexbox-item {
+  height: 100%;
 }
 </style>
