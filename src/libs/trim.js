e3c827a97f50504859b7c9e88abe10bc2a2f10d1
airyland <airyland@qq.com>
2016-03-23 19:47:31 +0800

    respect eslint

--- src/libs/trim.js
+++ src/libs/trim.js
@@ -3,8 +3,9 @@ export default function (str, replaceBreak) {
   str = str.replace(/^\s\s*/, '')
   var ws = /\s/
   var i = str.length
-  while (ws.test(str.charAt(--i)))
-  var rs = str.slice(0, i + 1)
+  while (ws.test(str.charAt(--i))) {
+    var rs = str.slice(0, i + 1)
+  }
   if (!rs) {
     return ''
   }
