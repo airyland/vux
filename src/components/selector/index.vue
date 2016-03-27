4ccc3a1cbee7ef8d5b6c760a029e05ab23526bcd
airyland <airyland@qq.com>
2016-03-28 01:34:56 +0800

    Selector: Refactor fix #62
    1.  remove selected prop
    2. rename `change` to `on-change`
    3. support plain array options

--- src/components/selector/index.vue
+++ src/components/selector/index.vue
@@ -1,34 +1,49 @@
 <template>
-	<div class="weui_cell" :class="{'weui_select_after':title, 'weui_cell_select':!readonly}">
-		<div class="weui_cell_hd" v-show="title" :class="{'weui_cell_primary':readonly}">
-      {{title}}
-    </div>
-    <div class="weui_cell_bd weui_cell_primary" v-if="!readonly">
-      <select class="weui_select" name="select1" v-model="value">
-      	<option value="" v-show="placeholder" selected="{{!selected}}">{{placeholder}}</option>
-        <option value="{{one.value}}" v-for="one in options" selected="{{selected && one.text===selected}}">{{one.text}}</option>
-      </select>
-    </div>
-	  <div class="weui_cell_ft" v-else>
-      {{selected}}
-    </div>
-	</div>
+<div class="weui_cell" :class="{'weui_select_after':title, 'weui_cell_select':!readonly}">
+	<div class="weui_cell_hd" v-show="title" :class="{'weui_cell_primary':readonly}">{{title}}</div>
+  <div class="weui_cell_bd weui_cell_primary" v-if="!readonly">
+    <select class="weui_select" v-model="value">
+    	<option value="" v-if="placeholder" :selected="placeholder && !value">{{placeholder}}</option>
+      <option :value="one.value" v-for="one in processOptions" :data-text="one.text" :data-value="value" :selected="one.value === value">{{one.text}}</option>
+    </select>
+  </div>
+  <div class="weui_cell_ft" v-else>
+    {{value | findByValue processOptions}}
+  </div>
+</div>
 </template>
 
 <script>
+import find from 'lodash.find'
+
+const findByValue = function (value, options) {
+  const _rs = find(options, function (item) {
+    return item.value === value
+  })
+  return _rs ? _rs.text : value
+}
+
 export default {
-  ready () {
-    this.value = this.selected
-  },
-  data: function () {
-    return {
-      values: null
+  computed: {
+    processOptions: function () {
+      if (this.options.length && this.options[0].text) {
+        return this.options
+      } else {
+        return this.options.map(function (item) {
+          return {
+            text: item,
+            value: item
+          }
+        })
+      }
     }
+  },
+  filters: {
+    findByValue
   },
   watch: {
     value: function (newValue) {
-      this.selected = newValue
-      this.$dispatch('change', newValue)
+      this.$dispatch('on-change', newValue)
     }
   },
   props: {
@@ -42,10 +57,6 @@ export default {
     },
     placeholder: {
       type: String
-    },
-    selected: {
-      type: String,
-      twoWay: true
     },
     readonly: {
       type: Boolean,
--- src/demos/Selector.vue
+++ src/demos/Selector.vue
@@ -5,7 +5,7 @@
     </group>
 
     <group title="with placeholder">
-      <selector placeholder="请选择省份" title="省份" :options="list" @change="change"></selector>
+      <selector placeholder="请选择省份" title="省份" :options="list" @on-change="onChange"></selector>
     </group>
 
     <group title="without title">
@@ -13,16 +13,20 @@
     </group>
     
     <group title="set value=广西">
-      <selector :selected.sync="selected1" :value.sync="value1" title="省份" :options="list"></selector>
+      <selector :value.sync="value1" title="省份" :options="list"></selector>
     </group>
 
-    <group title="readonly">
-      <selector selected="广东" :readonly=true title="省份" :options="list"></selector>
+    <group title="readonly, displays just like a cell">
+      <selector value="gd" readonly title="省份" :options="list"></selector>
+    </group>
+
+    <group title="use a plain options">
+      <selector value="C" title="Selector" :options="list1" @on-change="onChange"></selector>
     </group>
 
     <group title='multi selector'>
      <selector placeholder="请选择省份" title="省份" :options="list" type="inline"></selector>
-     <selector selected="广西" title="省份" :options="list"></selector>
+     <selector :value.sync="value2" title="省份" :options="list"></selector>
     </group>
   </div>
 </template>
@@ -38,12 +42,13 @@ export default {
   data: function () {
     return {
       list: [{ text: '广东', value: 'gd'}, {text: '广西', value: 'gx'}],
-      selected1: '广西',
-      value1: ''
+      value1: 'gx',
+      value2: 'gd',
+      list1: ['A', 'B', 'C']
     }
   },
   methods: {
-    change: function (val) {
+    onChange: function (val) {
       console.log(val)
     }
   }
