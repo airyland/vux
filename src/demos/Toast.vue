9319c5e35819d9b14f7fba290272954e5f95e49d
lichunqiang <light-li@hotmail.com>
2016-04-08 18:42:50 +0800

    Toast: add two type of icon

--- src/demos/Toast.vue
+++ src/demos/Toast.vue
@@ -2,9 +2,13 @@
   <div>
     <group>
       <switch title="默认" :value.sync="show"></switch>
+      <switch title="显示取消" :value.sync="show2"></switch>
+      <switch title="显示禁止" :value.sync="show3"></switch>
       <switch title="设置出现时间1s" :value.sync="show1"></switch>
     </group>
     <toast :show.sync="show">处理成功</toast>
+    <toast :show.sync="show2" type="cancel">取消操作</toast>
+    <toast :show.sync="show3" type="warn">禁止操作</toast>
     <toast :show.sync="show1" :time=1000>1s关闭</toast>
   </div>
 </template>
@@ -20,7 +24,9 @@ export default {
   data () {
     return {
       show: false,
-      show1: false
+      show1: false,
+      show2: false,
+      show3: false
     }
   }
 }
