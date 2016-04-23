d7bb9eed4195f3c52c1d1de744cc08cc1508722b
airyland <airyland@qq.com>
2016-04-23 22:36:12 +0800

    ButtonTab: Add an example with red dot

--- src/demos/Button-tab.vue
+++ src/demos/Button-tab.vue
@@ -10,16 +10,23 @@
       <button-tab-item selected>文章订阅</button-tab-item>
       <button-tab-item>商品订阅</button-tab-item>
     </button-tab>
+    <br>
+    <divider>Red Dot</divider>
+    <button-tab>
+      <button-tab-item selected>All Message</button-tab-item>
+      <button-tab-item><span class="vux-reddot-s">New Message</span></button-tab-item>
+    </button-tab>
   </div>
 </template>
 
 <script>
-import { ButtonTab, ButtonTabItem } from '../components'
+import { ButtonTab, ButtonTabItem, Divider } from '../components'
 
 export default {
   components: {
     ButtonTab,
-    ButtonTabItem
+    ButtonTabItem,
+    Divider
   }
 }
 </script>
