3e1dcb49d05d58a497943d7062ae51c332cbbf3f
airyland <airyland@qq.com>
2016-05-18 01:45:33 +0800

    Timeline: Fix style error on timeline-item changing. close #184

--- src/components/timeline/timeline-item.vue
+++ src/components/timeline/timeline-item.vue
@@ -10,63 +10,6 @@
 	</li>
 </template>
 
-<style lang="less">
-
-	@timeline: ~"timeline";
-
-	.@{timeline} {
-		&-item {
-			position:relative;
-		}
-
-		&-item-content {
-			padding:0 0 1.5rem 1.2rem;
-		}
-
-		&-item-head, &-item-head-first {
-			position:absolute;
-			content:'';
-			z-index:99;
-			border-radius:99px;
-		}
-
-		&-item-head {
-			width:10px;
-			height:10px;
-			left:1px;top:4px;
-		}
-
-		&-item-head-first {
-			width:20px;
-			height:20px;
-			left:-4px;top:5px;
-		}
-
-		&-item-tail {
-			position:absolute;
-			content:'';
-			height:100%;
-			width:2px;
-			left:5px;top:5px;
-		}
-
-		&-item-checked {
-			width: 100%;
-			position: absolute;
-			left: 0;
-			top: 45%;
-			transform: translateY(-50%);
-
-			&::before {
-				font-size: 12px;
-				width: 20px;
-				color: #FFF;
-			}
-		}
-	}
-
-</style>
-
 <script>
 import Icon from '../icon'
 
@@ -78,6 +21,16 @@ export default {
       headStyle: { backgroundColor: this.$parent.color }
     }
   },
+  ready () {
+    this.$parent.setChildProps()
+  },
+  beforeDestroy () {
+    // this will be null
+    const $parent = this.$parent
+    this.$nextTick(() => {
+      $parent.setChildProps()
+    })
+  },
   components: {
     Icon
   },
@@ -88,3 +41,4 @@ export default {
   }
 }
 </script>
+
