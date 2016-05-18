0f2b43e8e1f668bf58b4a0a89d6bf0ebe2c1dd13
airyland <airyland@qq.com>
2016-05-18 20:41:06 +0800

    Fix cell link

--- src/demos/Thanks.vue
+++ src/demos/Thanks.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <group title="Thx" title-color="#04be02">
-      <cell title="Router" is-link v-link="'https://github.com/vuejs/vue-router'">
+      <cell title="Router" link="https://github.com/vuejs/vue-router">
         <span class="demo-icon" slot="icon">&#xe636;</span>
         <span slot="value"><img src="http://cn.vuejs.org/images/logo.png" width="20" style="vertical-align: middle;padding-right: 5px;">Powered by Vue-router</span>
       </cell>
-      <cell title="Style" is-link v-link="'https://github.com/weui/weui'">
+      <cell title="Style" link="https://github.com/weui/weui">
         <span class="demo-icon" slot="icon">&#xe638;</span>
         <span slot="value"><span class="demo-icon">&#xe600</span>Based on Weui</span>
       </cell>
-      <cell title="Dev && Build" is-link v-link="'https://github.com/webpack/webpack'">
+      <cell title="Dev && Build" link="https://github.com/webpack/webpack">
         <span class="demo-icon" slot="icon" style="color: #000;">&#xe639;</span>
         <span slot="value"><img src="https://webpack.github.io/assets/favicon.png" width="20" style="vertical-align: middle;padding-right: 5px;">Webpack</span>
       </cell>
-      <cell title="Demo Icon" is-link v-link="'http://iconfont.cn/'">
+      <cell title="Demo Icon" link="http://iconfont.cn/">
         <span class="demo-icon" slot="icon">&#xe619;</span>
         <span slot="value"><img src="http://gtms04.alicdn.com/tps/i4/TB1_oz6GVXXXXaFXpXXJDFnIXXX-64-64.ico" width="20" style="vertical-align: middle;padding-right: 5px;">Iconfont</span>
       </cell>
