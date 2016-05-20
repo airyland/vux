abba3bce38fd45396437828db229fc48f930ab62
airyland <airyland@qq.com>
2016-05-20 16:24:32 +0800

    Datetime: Fix show issue on iOS8. close #183

--- src/components/datetime/index.vue
+++ src/components/datetime/index.vue
@@ -128,5 +128,124 @@ export default {
 </script>
 
 <style>
-@import './datetimepicker.css';
+.scroller-component {
+  display: block;
+  position: relative;
+  height: 238px;
+  overflow: hidden;
+  width: 100%;
+}
+
+.scroller-content {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+}
+
+.scroller-mask {
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100%;
+  margin: 0 auto;
+  width: 100%;
+  z-index: 3;
+  background-image:
+    -webkit-linear-gradient(top, rgba(255,255,255,0.95), rgba(255,255,255,0.6)),
+    -webkit-linear-gradient(bottom, rgba(255,255,255,0.95), rgba(255,255,255,0.6));
+  background-image:
+    linear-gradient(to bottom, rgba(255,255,255,0.95), rgba(255,255,255,0.6)),
+    linear-gradient(to top, rgba(255,255,255,0.95), rgba(255,255,255,0.6));
+  background-position: top, bottom;
+  background-size: 100% 102px;
+  background-repeat: no-repeat;
+}
+
+.scroller-item {
+  text-align: center;
+  font-size: 16px;
+  height: 34px;
+  line-height: 34px;
+  color: #000;
+}
+
+.scroller-indicator {
+  width: 100%;
+  height: 34px;
+  position: absolute;
+  left: 0;
+  top: 102px;
+  z-index: 3;
+  background-image:
+    -webkit-linear-gradient(top, #d0d0d0, #d0d0d0, transparent, transparent),
+    -webkit-linear-gradient(bottom, #d0d0d0, #d0d0d0, transparent, transparent);
+  background-image:
+    linear-gradient(to bottom, #d0d0d0, #d0d0d0, transparent, transparent),
+    linear-gradient(to top, #d0d0d0, #d0d0d0, transparent, transparent);
+  background-position: top, bottom;
+  background-size: 100% 1px;
+  background-repeat: no-repeat;
+}
+
+.dp-container {
+  position: fixed;
+  width: 100%;
+  left: 0;
+  bottom: 0;
+  z-index: 10000;
+  background-color: #fff;
+  display: none;
+  transition: transform 0.3s ease;
+  transform: translateY(100%);
+}
+
+.dp-mask {
+  z-index: 998;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0px;
+  top: 0px;
+  opacity: 0;
+  transition: opacity 0.1s ease;
+  background-color: #000;
+  z-index: 9999;
+}
+
+.dp-header {
+  display: flex;
+  width: 100%;
+  -webkit-box-align: center;
+  -webkit-align-items: center;
+  background-image: -webkit-linear-gradient(top, #e7e7e7, #e7e7e7, transparent, transparent);
+  background-image: linear-gradient(to bottom, #e7e7e7, #e7e7e7, transparent, transparent);
+  background-position: bottom;
+  background-size: 100% 1px;
+  background-repeat: no-repeat;
+}
+
+.dp-header .dp-item {
+  color: #04BE02;
+  font-size: 18px;
+  height: 44px;
+  line-height: 44px;
+  cursor: pointer;
+}
+
+.dp-content {
+  display: flex;
+  width: 100%;
+  box-align: center;
+  align-items: center;
+  padding: 10px 0;
+}
+
+.dp-header .dp-item,
+.dp-content .dp-item {
+  box-sizing: border-box;
+  flex: 1;
+  text-align: center;
+}
 </style>
