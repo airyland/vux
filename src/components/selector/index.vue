3c4ff5a034a2feba4f39d9afcd412a8a09913dc7
airyland <airyland@qq.com>
2016-05-29 17:17:50 +0800

    Selector: Fix label width

--- src/components/selector/index.vue
+++ src/components/selector/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="weui_cell" :class="{'weui_select_after':title, 'weui_cell_select':!readonly}">
-    <div class="weui_cell_hd" v-show="title" :class="{'weui_cell_primary':readonly}">{{title}}</div>
+    <div class="weui_cell_hd" v-show="title" :class="{'weui_cell_primary':readonly}">
+      <label for="" class="weui_label">{{title}}</label>
+    </div>
     <div class="weui_cell_bd weui_cell_primary" v-if="!readonly">
       <select class="weui_select" v-model="value">
         <option value="" v-if="placeholder" :selected="placeholder && !value">{{placeholder}}</option>
