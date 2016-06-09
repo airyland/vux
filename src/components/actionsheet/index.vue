0d2c221af19e79b403cc0b57c5039cd623730414
airyland <airyland@qq.com>
2016-06-09 16:27:19 +0800

    Actionsheet: Use on- for events, add short cut event, support title

--- src/components/actionsheet/index.vue
+++ src/components/actionsheet/index.vue
@@ -3,10 +3,10 @@
     <div class="weui_mask_transition" :class="{'weui_fade_toggle': show}" :style="{display: show ? 'block' : 'none'}" @click="show=false"></div>
     <div class="weui_actionsheet" :class="{'weui_actionsheet_toggle': show}">
       <div class="weui_actionsheet_menu">
-        <div class="weui_actionsheet_cell" v-for="(key, text) in menus" @click="emitEvent('menu-click', key)" v-html="text">
+        <div class="weui_actionsheet_cell" v-for="(key, text) in menus" @click="emitEvent('on-click-menu', key)" v-html="text">
         </div>
         <div class="vux-actionsheet-gap" v-if="showCancel"></div>
-        <div class="weui_actionsheet_cell vux-actionsheet-cancel" @click="emitEvent('menu-click', 'cancel')" v-if="showCancel">{{cancelText}}</div>
+        <div class="weui_actionsheet_cell vux-actionsheet-cancel" @click="emitEvent('on-click-menu', 'cancel')" v-if="showCancel">{{cancelText}}</div>
       </div>
     </div>
   </div>
@@ -36,8 +36,9 @@ export default {
   },
   methods: {
     emitEvent (event, menu) {
-      if (event === 'menu-click') {
+      if (event === 'on-click-menu' && !/.noop/.test(menu)) {
         this.$emit(event, menu)
+        this.$emit(`${event}-${menu}`)
         this.show = false
       }
     }
