844b063042f602742dc4e50c9978bfd383198b04
airyland <airyland@qq.com>
2016-06-25 23:20:29 +0800

    Add simple loading class util

---
+++ src/demos/Icon-loading.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <divider>default usage</divider>
+  <span class="vux-loading"></span>
+  <group>
+    <cell title="Cell Loading"><span class="vux-loading"></span></cell>
+    <x-input title="XInput loading" disabled :required="false" :show-clear="false">
+      <span class="vux-loading" slot="right"></span>
+    </x-input>
+  </group>
+</div>
+</template>
+
+<script>
+import { Group, Cell, Divider, XInput } from '../components'
+
+export default {
+  components: {
+    Group,
+    Cell,
+    Divider,
+    XInput
+  }
+}
+</script>
+
+<style>
+@import '../styles/loading.less';
+</style>
--- src/main.js
+++ src/main.js
@@ -79,6 +79,7 @@ import Dialog from './demos/Dialog'
 import DateFormatter from './demos/Date-formatter'
 import Card from './demos/Card'
 import Previewer from './demos/Previewer'
+import IconLoading from './demos/Icon-loading'
 
 const FastClick = require('fastclick')
 FastClick.attach(document.body)
@@ -319,6 +320,9 @@ router.map({
   },
   '/component/previewer': {
     component: Previewer
+  },
+  '/component/icon-loading': {
+    component: IconLoading
   }
 })
 
