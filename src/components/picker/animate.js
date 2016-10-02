81ca46bdd341e35addd0235c19914a9f2322b425
airyland <airyland@qq.com>
2016-10-02 15:41:37 +0800

    Picker: Add polyfill for requestAnimationFrame (close #586)

--- src/components/picker/animate.js
+++ src/components/picker/animate.js
@@ -7,11 +7,41 @@ let counter = 1
 let desiredFrames = 60
 let millisecondsPerSecond = 1000
 
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+// requestAnimationFrame polyfill by Erik Möller
+// fixes from Paul Irish and Tino Zijdel
+;(function () {
+  var lastTime = 0
+  var vendors = ['ms', 'moz', 'webkit', 'o']
+  for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame']
+    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame']
+  }
+
+  if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = function (callback, element) {
+      var currTime = new Date().getTime()
+      var timeToCall = Math.max(0, 16 - (currTime - lastTime))
+      var id = window.setTimeout(function () { callback(currTime + timeToCall) },
+            timeToCall)
+      lastTime = currTime + timeToCall
+      return id
+    }
+  }
+  if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = function (id) {
+      clearTimeout(id)
+    }
+  }
+}())
+
 export default {
 
   // A requestAnimationFrame wrapper / polyfill.
   requestAnimationFrame: (function () {
-    var requestFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame
+    var requestFrame = window.requestAnimationFrame
     return function (callback, root) {
       requestFrame(callback, root)
     }
