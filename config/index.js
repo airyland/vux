ba21a0bff0ebd9c7897cec99ae5b8612242167f2
airyland <airyland@qq.com>
2017-01-20 14:13:24 +0800

    build: better way to overwrite files

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../docs/demos/v2/index.html'),
-    assetsRoot: path.resolve(__dirname, '../docs/demos/v2'),
+    index: path.resolve(__dirname, '../docs/demos/temp/index.html'),
+    assetsRoot: path.resolve(__dirname, '../docs/demos/temp'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
     productionSourceMap: true,
