55c4eae02506c71a551b47e58dfd2f00909a4be2
airyland <airyland@qq.com>
2017-02-15 11:50:11 +0800

    group-title: Fix style missing. Close #916

--- src/components/group-title/index.vue
+++ src/components/group-title/index.vue
@@ -3,3 +3,8 @@
     <slot></slot>
   </div>
 </template>
+
+<style lang="less">
+@import '../../styles/weui/widget/weui_cell/weui_access';
+@import '../../styles/weui/widget/weui_cell/weui_cell_global';
+</style>
