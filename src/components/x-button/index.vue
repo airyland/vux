3552dfbf8e638844ae8f32474eb0636d289f7bc3
wg <247026287@qq.com>
2017-02-17 17:38:26 +0800

    x-button add loading styles

--- src/components/x-button/index.vue
+++ src/components/x-button/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <button class="weui_btn" :class="classes" :disabled="disabled" :type="actionType">
+  <button class="weui_btn" :class="classes" :disabled="btnDisabled" :type="actionType">
+    <i class="weui-loading" v-show="showLoading"></i>
     {{text}}<slot></slot>
   </button>
 </template>
@@ -14,18 +15,26 @@ export default {
     mini: Boolean,
     plain: Boolean,
     text: String,
-    actionType: String
+    actionType: String,
+    showLoading: {
+      type: Boolean,
+      default: false
+    }
   },
   computed: {
     classes () {
       return [
         {
-          weui_btn_disabled: this.disabled,
-          weui_btn_mini: this.mini
+          weui_btn_disabled: this.btnDisabled,
+          weui_btn_mini: this.mini,
+          weui_btn_loading: this.showLoading
         },
         `weui_btn_${this.type}`,
         this.plain ? `weui_btn_plain_${this.type}` : ''
       ]
+    },
+    btnDisabled () {
+      return this.showLoading ? true : this.disabled
     }
   }
 }
