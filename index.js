c5676b31b3abca6b67edc6789d42ccabe30b2f56
airyland <airyland@qq.com>
2017-02-22 21:35:51 +0800

    update main entry

--- index.js
+++ index.js
@@ -1,76 +1,79 @@
 // THIS FILE IS ONLY FOR IDE ENTRY CHECKING NOT FOR REAL USAGE
 
-import Array2stringFilter from './src/filters/array2String.js'
-import FormatTimeFilter from './src/filters/format-time.js'
-import FriendlyTimeFilter from './src/filters/friendly-time.js'
-import Name2valueFilter from './src/filters/name2value.js'
-import Value2nameFilter from './src/filters/value2name.js'
-import ChinaAddressV1Data from './src/datas/china_address_v1.json'
-import ChinaAddressData from './src/datas/china_address.json'
-import VuxComponentListData from './src/datas/vux_component_list.json'
-import AjaxPlugin from './src/plugins/ajax/index.js'
-import AlertPlugin from './src/plugins/alert/index.js'
-import ConfirmPlugin from './src/plugins/confirm/index.js'
-import DevicePlugin from './src/plugins/device/index.js'
-import LoadingPlugin from './src/plugins/loading/index.js'
-import TitlePlugin from './src/plugins/title/index.js'
-import ToastPlugin from './src/plugins/toast/index.js'
-import WechatPlugin from './src/plugins/wechat/index.js'
-import Base64Tool from './src/tools/base64/index.js'
-import base64 from './src/tools/base64/index.js'
-import CookieTool from './src/tools/cookie/index.js'
-import cookie from './src/tools/cookie/index.js'
-import Md5Tool from './src/tools/md5/index.js'
-import md5 from './src/tools/md5/index.js'
-import ClickOutsideDirective from './src/directives/click-outside/index.js'
-import InviewDirective from './src/directives/inview/index.js'
 import Actionsheet from './src/components/actionsheet/index.vue'
+import Agree from './src/components/agree/index.vue'
+import AjaxPlugin from './src/plugins/ajax/index.js'
 import Alert from './src/components/alert/index.vue'
+import AlertPlugin from './src/plugins/alert/index.js'
+import Array2stringFilter from './src/filters/array2String.js'
 import Badge from './src/components/badge/index.vue'
+import base64 from './src/tools/base64/index.js'
 import Blur from './src/components/blur/index.vue'
 import Box from './src/components/box/index.vue'
-import ButtonTabItem from './src/components/button-tab/button-tab-item.vue'
 import ButtonTab from './src/components/button-tab/button-tab.vue'
+import ButtonTabItem from './src/components/button-tab/button-tab-item.vue'
 import Calendar from './src/components/calendar/index.vue'
 import Card from './src/components/card/index.vue'
 import Cell from './src/components/cell/index.vue'
-import CheckIcon from './src/components/check-icon/index.vue'
-import CheckerItem from './src/components/checker/checker-item.vue'
 import Checker from './src/components/checker/checker.vue'
+import CheckerItem from './src/components/checker/checker-item.vue'
+import CheckIcon from './src/components/check-icon/index.vue'
 import Checklist from './src/components/checklist/index.vue'
+import ChinaAddressData from './src/datas/china_address.json'
+import ChinaAddressV1Data from './src/datas/china_address_v1.json'
+import ClickOutsideDirective from './src/directives/click-outside/index.js'
 import Clocker from './src/components/clocker/index.vue'
 import ColorPicker from './src/components/color-picker/index.vue'
 import Confirm from './src/components/confirm/index.vue'
+import ConfirmPlugin from './src/plugins/confirm/index.js'
+import cookie from './src/tools/cookie/index.js'
 import Countdown from './src/components/countdown/index.vue'
 import Countup from './src/components/countup/index.vue'
+import dateFormat from './src/tools/date/format.js'
 import Datetime from './src/components/datetime/index.vue'
+import debounce from './src/tools/debounce/index.js'
+import Demobasic from './src/components/fullpage/DemoBasic.vue'
+import Demoindex from './src/components/popover/DemoIndex.vue'
+import DevicePlugin from './src/plugins/device/index.js'
 import DevTip from './src/components/dev-tip/index.vue'
 import Divider from './src/components/divider/index.vue'
-import FlexboxItem from './src/components/flexbox/flexbox-item.vue'
 import Flexbox from './src/components/flexbox/flexbox.vue'
+import FlexboxItem from './src/components/flexbox/flexbox-item.vue'
+import FormatTimeFilter from './src/filters/format-time.js'
 import FormPreview from './src/components/form-preview/index.vue'
-import Demobasic from './src/components/fullpage/DemoBasic.vue'
+import FriendlyTimeFilter from './src/filters/friendly-time.js'
 import Fullpage from './src/components/fullpage/index.vue'
-import GridItem from './src/components/grid/grid-item.vue'
 import Grid from './src/components/grid/grid.vue'
-import GroupTitle from './src/components/group-title/index.vue'
+import GridItem from './src/components/grid/grid-item.vue'
 import Group from './src/components/group/index.vue'
+import GroupTitle from './src/components/group-title/index.vue'
 import Icon from './src/components/icon/index.vue'
 import InlineCalendar from './src/components/inline-calendar/index.vue'
 import InlineDesc from './src/components/inline-desc/index.vue'
 import InlineXNumber from './src/components/inline-x-number/index.vue'
-import LoadMore from './src/components/load-more/index.vue'
+import InviewDirective from './src/directives/inview/index.js'
 import Loading from './src/components/loading/index.vue'
+import LoadingPlugin from './src/plugins/loading/index.js'
+import LoadMore from './src/components/load-more/index.vue'
+import LocalePlugin from './src/plugins/locale/index.js'
+import Marquee from './src/components/marquee/marquee.vue'
+import MarqueeItem from './src/components/marquee/marquee-item.vue'
 import Masker from './src/components/masker/index.vue'
+import md5 from './src/tools/md5/index.js'
+import Msg from './src/components/msg/index.vue'
+import Name2valueFilter from './src/filters/name2value.js'
+import numberComma from './src/tools/number/comma.js'
+import numberPad from './src/tools/number/pad.js'
+import numberRandom from './src/tools/number/random.js'
 import NumberRoller from './src/components/number-roller/index.vue'
 import Panel from './src/components/panel/index.vue'
 import Picker from './src/components/picker/index.vue'
-import Demoindex from './src/components/popover/DemoIndex.vue'
 import Popover from './src/components/popover/index.vue'
-import PopupPicker from './src/components/popup-picker/index.vue'
 import Popup from './src/components/popup/index.vue'
+import PopupPicker from './src/components/popup-picker/index.vue'
 import Previewer from './src/components/previewer/index.vue'
 import Qrcode from './src/components/qrcode/index.vue'
+import querystring from './src/tools/querystring/index.js'
 import Radio from './src/components/radio/index.vue'
 import Range from './src/components/range/index.vue'
 import Rater from './src/components/rater/index.vue'
@@ -79,30 +82,40 @@ import Search from './src/components/search/index.vue'
 import Selector from './src/components/selector/index.vue'
 import Shake from './src/components/shake/index.vue'
 import Spinner from './src/components/spinner/index.vue'
-import StepItem from './src/components/step/step-item.vue'
 import Step from './src/components/step/step.vue'
+import StepItem from './src/components/step/step-item.vue'
 import Sticky from './src/components/sticky/index.vue'
+import stringTrim from './src/tools/string/trim.js'
+import Swipeout from './src/components/swipeout/swipeout.vue'
 import SwipeoutButton from './src/components/swipeout/swipeout-button.vue'
 import SwipeoutItem from './src/components/swipeout/swipeout-item.vue'
-import Swipeout from './src/components/swipeout/swipeout.vue'
-import SwiperItem from './src/components/swiper/swiper-item.vue'
 import Swiper from './src/components/swiper/swiper.vue'
-import TabItem from './src/components/tab/tab-item.vue'
+import SwiperItem from './src/components/swiper/swiper-item.vue'
 import Tab from './src/components/tab/tab.vue'
-import TabbarItem from './src/components/tabbar/tabbar-item.vue'
 import Tabbar from './src/components/tabbar/tabbar.vue'
-import TimelineItem from './src/components/timeline/timeline-item.vue'
+import TabbarItem from './src/components/tabbar/tabbar-item.vue'
+import TabItem from './src/components/tab/tab-item.vue'
+import throttle from './src/tools/throttle/index.js'
 import Timeline from './src/components/timeline/timeline.vue'
+import TimelineItem from './src/components/timeline/timeline-item.vue'
 import Tip from './src/components/tip/index.vue'
+import TitlePlugin from './src/plugins/title/index.js'
 import Toast from './src/components/toast/index.vue'
+import ToastPlugin from './src/plugins/toast/index.js'
+import TransferDomDirective from './src/directives/transfer-dom/index.js'
+import Value2nameFilter from './src/filters/value2name.js'
 import Video from './src/components/video/index.vue'
 import ViewBox from './src/components/view-box/index.vue'
+import VuxComponentListData from './src/datas/vux_component_list.json'
 import WechatEmotion from './src/components/wechat-emotion/index.vue'
+import WechatPlugin from './src/plugins/wechat/index.js'
+import WepayInput from './src/components/wepay-input/index.vue'
 import XAddress from './src/components/x-address/index.vue'
 import XButton from './src/components/x-button/index.vue'
 import XCircle from './src/components/x-circle/index.vue'
 import XDialog from './src/components/x-dialog/index.vue'
 import XHeader from './src/components/x-header/index.vue'
+import XHr from './src/components/x-hr/index.vue'
 import XImg from './src/components/x-img/index.vue'
 import XInput from './src/components/x-input/index.vue'
 import XNumber from './src/components/x-number/index.vue'
@@ -111,77 +124,80 @@ import XSwitch from './src/components/x-switch/index.vue'
 import XTextarea from './src/components/x-textarea/index.vue'
 
 export {
-  Array2stringFilter,
-  FormatTimeFilter,
-  FriendlyTimeFilter,
-  Name2valueFilter,
-  Value2nameFilter,
-  ChinaAddressV1Data,
-  ChinaAddressData,
-  VuxComponentListData,
-  AjaxPlugin,
-  AlertPlugin,
-  ConfirmPlugin,
-  DevicePlugin,
-  LoadingPlugin,
-  TitlePlugin,
-  ToastPlugin,
-  WechatPlugin,
-  Base64Tool,
-  base64,
-  CookieTool,
-  cookie,
-  Md5Tool,
-  md5,
-  ClickOutsideDirective,
-  InviewDirective,
   Actionsheet,
+  Agree,
+  AjaxPlugin,
   Alert,
+  AlertPlugin,
+  Array2stringFilter,
   Badge,
+  base64,
   Blur,
   Box,
-  ButtonTabItem,
   ButtonTab,
+  ButtonTabItem,
   Calendar,
   Card,
   Cell,
-  CheckIcon,
-  CheckerItem,
   Checker,
+  CheckerItem,
+  CheckIcon,
   Checklist,
+  ChinaAddressData,
+  ChinaAddressV1Data,
+  ClickOutsideDirective,
   Clocker,
   ColorPicker,
   Confirm,
+  ConfirmPlugin,
+  cookie,
   Countdown,
   Countup,
+  dateFormat,
   Datetime,
+  debounce,
+  Demobasic,
+  Demoindex,
+  DevicePlugin,
   DevTip,
   Divider,
-  FlexboxItem,
   Flexbox,
+  FlexboxItem,
+  FormatTimeFilter,
   FormPreview,
-  Demobasic,
+  FriendlyTimeFilter,
   Fullpage,
-  GridItem,
   Grid,
-  GroupTitle,
+  GridItem,
   Group,
+  GroupTitle,
   Icon,
   InlineCalendar,
   InlineDesc,
   InlineXNumber,
-  LoadMore,
+  InviewDirective,
   Loading,
+  LoadingPlugin,
+  LoadMore,
+  LocalePlugin,
+  Marquee,
+  MarqueeItem,
   Masker,
+  md5,
+  Msg,
+  Name2valueFilter,
+  numberComma,
+  numberPad,
+  numberRandom,
   NumberRoller,
   Panel,
   Picker,
-  Demoindex,
   Popover,
-  PopupPicker,
   Popup,
+  PopupPicker,
   Previewer,
   Qrcode,
+  querystring,
   Radio,
   Range,
   Rater,
@@ -190,30 +206,40 @@ export {
   Selector,
   Shake,
   Spinner,
-  StepItem,
   Step,
+  StepItem,
   Sticky,
+  stringTrim,
+  Swipeout,
   SwipeoutButton,
   SwipeoutItem,
-  Swipeout,
-  SwiperItem,
   Swiper,
-  TabItem,
+  SwiperItem,
   Tab,
-  TabbarItem,
   Tabbar,
-  TimelineItem,
+  TabbarItem,
+  TabItem,
+  throttle,
   Timeline,
+  TimelineItem,
   Tip,
+  TitlePlugin,
   Toast,
+  ToastPlugin,
+  TransferDomDirective,
+  Value2nameFilter,
   Video,
   ViewBox,
+  VuxComponentListData,
   WechatEmotion,
+  WechatPlugin,
+  WepayInput,
   XAddress,
   XButton,
   XCircle,
   XDialog,
   XHeader,
+  XHr,
   XImg,
   XInput,
   XNumber,
