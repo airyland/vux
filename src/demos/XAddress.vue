60fc40aba971fce65e4e29e931e89f34f081e0df
airyland <airyland@qq.com>
2017-04-02 01:13:29 +0800

    x-address: Upgrade data

--- src/demos/XAddress.vue
+++ src/demos/XAddress.vue
@@ -31,7 +31,7 @@
 </template>
 
 <script>
-import { Group, XAddress, ChinaAddressData, XButton, Cell, Value2nameFilter as value2name } from 'vux'
+import { Group, XAddress, ChinaAddressV3Data, XButton, Cell, Value2nameFilter as value2name } from 'vux'
 
 export default {
   components: {
@@ -47,7 +47,7 @@ export default {
       title2: '设置值',
       value2: ['天津市', '市辖区', '和平区'],
       value3: ['广东省', '中山市', '--'],
-      addressData: ChinaAddressData,
+      addressData: ChinaAddressV3Data,
       value4: [],
       value5: ['广东省', '深圳 市', '南山区']
     }
@@ -57,7 +57,7 @@ export default {
       this.value2 = ['430000', '430400', '430407']
     },
     getName (value) {
-      return value2name(value, ChinaAddressData)
+      return value2name(value, ChinaAddressV3Data)
     },
     logHide (str) {
       console.log('on-hide', str)
