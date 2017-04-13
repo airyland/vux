708886ae2e01eaee11e505a41cc4bace1704bbea
airyland <airyland@qq.com>
2017-04-13 22:03:50 +0800

    radio: Move props to files for sharing with wrapping components

---
+++ src/components/radio/props.js
@@ -0,0 +1,23 @@
+// used by radio and popup-radio
+
+export default function () {
+  return {
+    options: {
+      type: Array,
+      required: true
+    },
+    value: [String, Number],
+    fillMode: {
+      type: Boolean,
+      default: false
+    },
+    fillPlaceholder: {
+      type: String,
+      default: '其他'
+    },
+    fillLabel: {
+      type: String,
+      default: '其他'
+    }
+  }
+}
