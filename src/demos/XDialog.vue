86728f61271a16e2493e6bba98826c0b7659380e
airyland <airyland@qq.com>
2017-04-16 01:38:01 +0800

    x-dialog: Update demo i18n

--- src/demos/XDialog.vue
+++ src/demos/XDialog.vue
@@ -1,42 +1,45 @@
 <template>
   <div style="height: 1000px">
     <group>
-      <x-switch v-model="show" title="Toggle"></x-switch>
-      <x-switch v-model="showHideOnBlur" title="点击遮罩自动关闭"></x-switch>
+      <x-switch v-model="show" :title="$t('Toggle')"></x-switch>
+      <x-switch v-model="showHideOnBlur" :title="$t('hide on clicking mask')"></x-switch>
     </group>
     <group style="padding-top: 300px">
-      <x-switch v-model="showNoScroll" title="背景不可滚动"></x-switch>
+      <x-switch v-model="showNoScroll" :title="$t('disable background scrolling')"></x-switch>
     </group>
     <div v-transfer-dom>
       <x-dialog v-model="show" class="dialog-demo">
-        <p class="dialog-title">I'm a bg scroll Dialog.</p>
         <div class="img-box">
           <img src="../assets/demo/dialog/01.jpg" style="max-width:100%">
         </div>
-        <span class="vux-close" @click="show=false"></span>
+        <div @click="show=false">
+          <span class="vux-close"></span>
+        </div>
       </x-dialog>
     </div>
     <div v-transfer-dom>
       <x-dialog v-model="showHideOnBlur" class="dialog-demo" :hideOnBlur="true">
-        <p class="dialog-title">I'm a bg scroll Dialog.</p>
         <div class="img-box">
           <img src="../assets/demo/dialog/01.jpg" style="max-width:100%">
         </div>
-        <span class="vux-close" @click="showHideOnBlur=false"></span>
+        <div @click="showHideOnBlur=false">
+          <span class="vux-close"></span>
+        </div>
       </x-dialog>
     </div>
     <div v-transfer-dom>
       <x-dialog v-model="showNoScroll" class="dialog-demo" :scroll="false">
-        <p class="dialog-title">I'm a no scroll Dialog.</p>
         <div class="img-box">
           <img src="../assets/demo/dialog/01.jpg" style="max-width:100%">
         </div>
-        <span class="vux-close" @click="showNoScroll=false"></span>
+        <div @click="showNoScroll=false">
+          <span class="vux-close"></span>
+        </div>
       </x-dialog>
     </div>
 
     <group style="padding-top: 300px">
-      <x-switch v-model="showScrollBox" title="主体内容滚动"></x-switch>
+      <x-switch v-model="showScrollBox" :title="$t('long long content')"></x-switch>
     </group>
     <div v-transfer-dom>
       <x-dialog v-model="showScrollBox" class="dialog-demo">
@@ -44,12 +47,25 @@
         <div class="img-box" style="height:100px;padding:15px 0;overflow:scroll;-webkit-overflow-scrolling:touch;">
           <p v-for="i in 20">{{i}}</p>
         </div>
-        <span class="vux-close" @click="showScrollBox=false"></span>
+        <div @click="showScrollBox=false">
+          <span class="vux-close"></span>
+        </div>
       </x-dialog>
     </div>
 
   </div>
 </template>
+
+<i18n>
+hide on clicking mask:
+  zh-CN: 点击遮罩自动关闭
+Toggle:
+  zh-CN: 显示/隐藏
+disable background scrolling:
+  zh-CN: 背景不可滚动
+long long content:
+  zh-CN: 很长很长的内容
+</i18n>
 
 <script>
 import { XDialog, XButton, Group, XSwitch, TransferDomDirective as TransferDom } from 'vux'
@@ -77,6 +93,7 @@ export default {
 
 <style lang="less" scoped>
 @import '~vux/src/styles/close';
+
 .dialog-demo {
   .weui-dialog{
     border-radius: 8px;
