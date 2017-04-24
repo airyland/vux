efaa1d3ef8a19601a6417723bebf2d3ec59ce119
airyland <airyland@qq.com>
2017-04-24 18:35:49 +0800

    search: Update document. Close #1299

--- src/demos/Search.vue
+++ src/demos/Search.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
     <img src="../assets/demo/filter_bg.jpg" style="width: 100%">
-    <search @result-click="resultClick" @on-change="getResult" :results="results" v-model="value" position="absolute" auto-scroll-to-top top="46px" @on-focus="onFocus" @on-cancel="onCancel" @on-submit="onSubmit"></search>
-    <br>
+    <search
+    @result-click="resultClick"
+    @on-change="getResult"
+    :results="results"
+    v-model="value"
+    position="absolute"
+    auto-scroll-to-top top="46px"
+    @on-focus="onFocus"
+    @on-cancel="onCancel"
+    @on-submit="onSubmit"
+    ref="search"></search>
+    <div style="padding:15px;">
+      <x-button @click.native="setFocus" type="primary">set focus</x-button>
+    </div>
     <group>
       <cell title="static position demo" is-link link="/component/search-static"></cell>
     </group>
@@ -10,15 +22,19 @@
 </template>
 
 <script>
-import { Search, Group, Cell } from 'vux'
+import { Search, Group, Cell, XButton } from 'vux'
 
 export default {
   components: {
     Search,
     Group,
-    Cell
+    Cell,
+    XButton
   },
   methods: {
+    setFocus () {
+      this.$refs.search.setFocus()
+    },
     resultClick (item) {
       window.alert('you click the result item: ' + JSON.stringify(item))
     },
