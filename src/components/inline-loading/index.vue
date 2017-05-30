10b7ee2819c0386ec8d5afdc52314352d59b6f4e
airyland <airyland@qq.com>
2017-05-31 01:10:23 +0800

    new component: inline-loading

---
+++ src/components/inline-loading/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <i class="weui-loading"></i>
+</template>
+
+<style lang="less">
+@import '../../styles/weui/widget/weui-loading/weui-loading.less';
+</style>
---
+++ src/demos/InlineLoading.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <br>
+    <p style="text-align:center;">
+      <inline-loading></inline-loading>
+    </p>
+    <br>
+    <p style="text-align:center;">
+      <span style="vertical-align:middle;display:inline-block;font-size:14px;">{{ $t('loading') }}&nbsp;&nbsp;</span><inline-loading></inline-loading>
+    </p>
+    <br>
+    <p style="text-align:center;">
+      <inline-loading></inline-loading><span style="vertical-align:middle;display:inline-block;font-size:14px;">&nbsp;&nbsp;{{ $t('loading') }}</span>
+    </p>
+  </div>
+</template>
+
+<i18n>
+loading:
+  zh-CN: 加载中
+</i18n>
+
+<script>
+import { InlineLoading } from 'vux'
+
+export default {
+  components: {
+    InlineLoading
+  }
+}
+</script>
