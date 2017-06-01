7705b0912554286e6e03d21a1daeb5a23ed66fe4
airyland <airyland@qq.com>
2017-06-01 19:32:20 +0800

    inline-calendar: Support slot:each-day

---
+++ src/demos/InlineCalendarEachDaySlot.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <inline-calendar>
+      <template slot="each-day" scope="props">
+        <span v-show="props.isShow">{{ getStr(props.child.day) }}</span>
+      </template>
+    </inline-calendar>
+  </div>
+</template>
+
+<script>
+import { InlineCalendar } from 'vux'
+
+const listMap = {
+  '8': '捌',
+  '6': '陆',
+  '9': '玖'
+}
+export default {
+  components: {
+    InlineCalendar
+  },
+  methods: {
+    getStr (day) {
+      return listMap[day] || day
+    }
+  }
+}
+</script>
