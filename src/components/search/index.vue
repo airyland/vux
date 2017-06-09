189507f5bfdeabc6f6aa9bb7d2dc8f5b9e55c072
airyland <airyland@qq.com>
2017-06-09 17:03:12 +0800

    search: Support slot:left

--- src/components/search/index.vue
+++ src/components/search/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="vux-search-box" :class="{'vux-search-fixed':isFixed}" :style="{top: isFixed ? top : '', position: fixPosition }">
     <div class="weui-search-bar" :class="{'weui-search-bar_focusing': !isCancel || currentValue}">
+      <slot name="left"></slot>
       <form class="weui-search-bar__form" @submit.prevent="$emit('on-submit', value)" action=".">
         <div class="vux-search-mask" @click="touch" v-show="!isFixed && autoFixed"></div>
         <div class="weui-search-bar__box">
