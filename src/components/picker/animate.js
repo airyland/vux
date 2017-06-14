39c9471e1bd6bc29234b5798f6d680db5eba7bad
airyland <airyland@qq.com>
2017-06-14 12:03:56 +0800

    picker: fix window issue in ssr

--- src/components/picker/animate.js
+++ src/components/picker/animate.js
@@ -12,38 +12,42 @@ let millisecondsPerSecond = 1000
 
 // requestAnimationFrame polyfill by Erik Möller
 // fixes from Paul Irish and Tino Zijdel
-;(function () {
-  var lastTime = 0
-  var vendors = ['ms', 'moz', 'webkit', 'o']
-  for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame']
-    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame']
-  }
+if (typeof window !== 'undefined') {
+  ;(function () {
+    var lastTime = 0
+    var vendors = ['ms', 'moz', 'webkit', 'o']
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+      window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame']
+      window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame']
+    }
 
-  if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = function (callback, element) {
-      var currTime = new Date().getTime()
-      var timeToCall = Math.max(0, 16 - (currTime - lastTime))
-      var id = window.setTimeout(function () { callback(currTime + timeToCall) },
-            timeToCall)
-      lastTime = currTime + timeToCall
-      return id
+    if (!window.requestAnimationFrame) {
+      window.requestAnimationFrame = function (callback, element) {
+        var currTime = new Date().getTime()
+        var timeToCall = Math.max(0, 16 - (currTime - lastTime))
+        var id = window.setTimeout(function () { callback(currTime + timeToCall) },
+              timeToCall)
+        lastTime = currTime + timeToCall
+        return id
+      }
     }
-  }
-  if (!window.cancelAnimationFrame) {
-    window.cancelAnimationFrame = function (id) {
-      clearTimeout(id)
+    if (!window.cancelAnimationFrame) {
+      window.cancelAnimationFrame = function (id) {
+        clearTimeout(id)
+      }
     }
-  }
-}())
+  }())
+}
 
 module.exports = {
 
   // A requestAnimationFrame wrapper / polyfill.
   requestAnimationFrame: (function () {
-    var requestFrame = window.requestAnimationFrame
-    return function (callback, root) {
-      requestFrame(callback, root)
+    if (typeof window !== 'undefined') {
+      var requestFrame = window.requestAnimationFrame
+      return function (callback, root) {
+        requestFrame(callback, root)
+      }
     }
   })(),
 
