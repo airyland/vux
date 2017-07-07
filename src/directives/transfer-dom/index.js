83f9eb321bbb35f20e4e1800d2e04580b50dc92d
airyland <airyland@qq.com>
2017-07-07 16:47:28 +0800

    transfer-dom: Do not re-render on event:componentUpdated

--- src/directives/transfer-dom/index.js
+++ src/directives/transfer-dom/index.js
@@ -8,10 +8,36 @@ import objectAssign from 'object-assign'
  */
 function getTarget (node) {
   if (node === void 0) {
-    node = document.body
+    return document.body
   }
-  if (node === true) { return document.body }
+
+  if (typeof node === 'string' && node.indexOf('?') === 0) {
+    return document.body
+  } else if (typeof node === 'string' && node.indexOf('?') > 0) {
+    node = node.split('?')[0]
+  }
+
+  if (node === 'body' || node === true) {
+    return document.body
+  }
+
   return node instanceof window.Node ? node : document.querySelector(node)
+}
+
+function getShouldUpdate (node) {
+  // do not updated by default
+  if (!node) {
+    return false
+  }
+  if (typeof node === 'string' && node.indexOf('?') > 0) {
+    try {
+      const config = JSON.parse(node.split('?')[1])
+      return config.autoUpdate || false
+    } catch (e) {
+      return false
+    }
+  }
+  return false
 }
 
 const directive = {
@@ -36,6 +62,10 @@ const directive = {
     }
   },
   componentUpdated (el, { value }) {
+    const shouldUpdate = getShouldUpdate(value)
+    if (!shouldUpdate) {
+      return
+    }
     // need to make sure children are done updating (vs. `update`)
     var ref$1 = el.__transferDomData
     // homes.get(el)
