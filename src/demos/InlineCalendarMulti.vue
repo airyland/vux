7ca6abd9b3b15816f0a7dd17829a4bdd2aa29702
airyland <airyland@qq.com>
2017-07-30 16:54:26 +0800

    inline-calendar: Add demo

---
+++ src/demos/InlineCalendarMulti.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <inline-calendar v-model="value"></inline-calendar>
+    <br>
+    {{ value }}
+    <div style="padding:15px;">
+      <x-button type="primary" @click.native="updateValue">update value</x-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { InlineCalendar, dateFormat, XButton } from 'vux'
+
+const today = dateFormat(new Date(), 'YYYY-MM-DD')
+const yesterday = dateFormat(new Date().getTime() - 24 * 3600 * 1000, 'YYYY-MM-DD')
+
+export default {
+  components: {
+    InlineCalendar,
+    XButton
+  },
+  data () {
+    return {
+      value: [yesterday, today]
+    }
+  },
+  methods: {
+    updateValue () {
+      if (this.value.length === 1) {
+        this.value = [yesterday, today]
+      } else {
+        this.value = [today]
+      }
+    }
+  }
+}
+</script>
