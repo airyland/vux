34834e982da3add893582a6e493a50a80d3c0ccc
airyland <airyland@qq.com>
2017-09-22 17:13:30 +0800

    demo: update main.js

--- src/main.js
+++ src/main.js
@@ -1,24 +1,49 @@
+import objectAssign from 'object-assign'
+
 import Vue from 'vue'
+
+import App from './App'
+
+import Vuex from 'vuex'
+import vuexI18n from 'vuex-i18n'
 import VueRouter from 'vue-router'
 import { sync } from 'vuex-router-sync'
 
 Vue.use(VueRouter)
-import App from './App'
-import Vuex from 'vuex'
 Vue.use(Vuex)
-
-import vuexI18n from 'vuex-i18n'
 
 require('es6-promise').polyfill()
 
-/**
-* you can add your module here
-*/
+/** i18n **/
 let store = new Vuex.Store({
   modules: {
     i18n: vuexI18n.store
   }
 })
+
+Vue.use(vuexI18n.plugin, store)
+
+const vuxLocales = require('json-loader!yaml-loader!vux/src/locales/all.yml')
+const componentsLocales = require('json-loader!yaml-loader!src/locales/components.yml')
+
+const finalLocales = {
+  'en': objectAssign(vuxLocales['en'], componentsLocales['en']),
+  'zh-CN': objectAssign(vuxLocales['zh-CN'], componentsLocales['zh-CN'])
+}
+
+for (let i in finalLocales) {
+  Vue.i18n.add(i, finalLocales[i])
+}
+
+import { DatetimePlugin, CloseDialogsPlugin, ConfigPlugin, BusPlugin, LocalePlugin, DevicePlugin, ToastPlugin, AlertPlugin, ConfirmPlugin, LoadingPlugin, WechatPlugin, AjaxPlugin, AppPlugin } from 'vux'
+
+Vue.use(LocalePlugin)
+const nowLocale = Vue.locale.get()
+if (/zh/.test(nowLocale)) {
+  Vue.i18n.set('zh-CN')
+} else {
+  Vue.i18n.set('en')
+}
 
 store.registerModule('vux', {
   state: {
@@ -44,11 +69,12 @@ store.registerModule('vux', {
   }
 })
 
-Vue.use(vuexI18n.plugin, store)
+// global VUX config
+Vue.use(ConfigPlugin, {
+  $layout: 'VIEW_BOX' // global config for VUX, since v2.5.12
+})
 
 // plugins
-import { ConfigPlugin, BusPlugin, DatetimePlugin, LocalePlugin, DevicePlugin, ToastPlugin, AlertPlugin, ConfirmPlugin, LoadingPlugin, WechatPlugin, AjaxPlugin, AppPlugin } from 'vux'
-
 Vue.use(DevicePlugin)
 Vue.use(ToastPlugin)
 Vue.use(AlertPlugin)
@@ -56,12 +82,8 @@ Vue.use(ConfirmPlugin)
 Vue.use(LoadingPlugin)
 Vue.use(WechatPlugin)
 Vue.use(AjaxPlugin)
-Vue.use(LocalePlugin)
-Vue.use(DatetimePlugin)
 Vue.use(BusPlugin)
-Vue.use(ConfigPlugin, {
-  $layout: 'VIEW_BOX' // global config for VUX, since v2.5.12
-})
+Vue.use(DatetimePlugin)
 
 // test
 if (process.env.platform === 'app') {
@@ -101,27 +123,6 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-import objectAssign from 'object-assign'
-
-const vuxLocales = require('json-loader!yaml-loader!vux/src/locales/all.yml')
-const componentsLocales = require('json-loader!yaml-loader!src/locales/components.yml')
-
-const finalLocales = {
-  'en': objectAssign(vuxLocales['en'], componentsLocales['en']),
-  'zh-CN': objectAssign(vuxLocales['zh-CN'], componentsLocales['zh-CN'])
-}
-
-for (let i in finalLocales) {
-  Vue.i18n.add(i, finalLocales[i])
-}
-
-const nowLocale = Vue.locale.get()
-if (/zh/.test(nowLocale)) {
-  Vue.i18n.set('zh-CN')
-} else {
-  Vue.i18n.set('en')
-}
-
 const FastClick = require('fastclick')
 FastClick.attach(document.body)
 
@@ -131,6 +132,8 @@ const routes = []
 const router = new VueRouter({
   routes
 })
+
+Vue.use(CloseDialogsPlugin, router)
 
 sync(store, router)
 
