441fc18526d71e2c8354f2883143e5339bab272d
emondora <leespirit650@gmail.com>
2017-09-25 15:11:40 +0800

    XInput: update "firstError" check
    Remove "firstError" check when "isType equals function". 
    As a result of that, you can get the latest error message show in the "Toast".
    Why? 
    Sometimes we want to tells the user much more error details about there typing, more than "the first error". 
    For Instance: "Identity No" check, "Amount” check.

--- src/components/x-input/index.vue
+++ src/components/x-input/index.vue
@@ -385,9 +385,7 @@ export default {
         if (!this.valid) {
           this.errors.format = validStatus.msg
           this.forceShowError = true
-          if (!this.firstError) {
-            this.getError()
-          }
+          this.getError()
           return
         } else {
           delete this.errors.format
