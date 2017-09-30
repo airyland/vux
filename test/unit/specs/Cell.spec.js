f627b3c7caa317afe73076e929bf5870d4a38aa9
airyland <airyland@qq.com>
2017-09-30 12:13:12 +0800

    test: try vue-test-utils

--- test/unit/specs/Cell.spec.js
+++ test/unit/specs/Cell.spec.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
 import Cell from '@/components/cell/index.vue'
+
+import { mount } from 'vue-test-utils'
+import { expect } from 'chai'
 
 describe('Cell.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Cell)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.tagName)
-      .to.equal('DIV')
+    const wrapper = mount(Cell)
+    expect(wrapper.contains('div')).to.equal(true)
   })
 })
