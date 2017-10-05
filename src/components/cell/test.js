e3bfc418a49172363ade9ec900d70e2adbef0bf5
airyland <airyland@qq.com>
2017-10-05 15:03:24 +0800

    cell: fix test

--- src/components/cell/test.js
+++ src/components/cell/test.js
@@ -1,11 +1,14 @@
-import Cell from './index.vue'
+import CellWrapper from './test/wrapper'
 
 import { mount } from 'vue-test-utils'
 import { expect } from 'chai'
 
 describe('Cell.vue', () => {
-  it('should render correct contents', () => {
-    const wrapper = mount(Cell)
+  it('should render correct value', () => {
+    const wrapper = mount(CellWrapper)
+    wrapper.setProps({
+      value: 'hello'
+    })
     expect(wrapper.contains('div')).to.equal(true)
   })
 })
