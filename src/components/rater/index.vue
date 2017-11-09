9f45c216a4645ff591ea6aed710200617cc493e9
zhumingpeng <zhumingpeng@bbktel.com>
2017-11-09 18:54:49 +0800

    rater组件star属性支持v-html渲染

--- src/components/rater/index.vue
+++ src/components/rater/index.vue
@@ -2,7 +2,7 @@
   <div class="vux-rater">
     <input v-model="currentValue" style="display:none">
     <a class="vux-rater-box" v-for="i in max" @click="handleClick(i-1)" :class="{'is-active':currentValue > i-1}" :style="{color: colors && colors[i-1] ? colors[i-1] : '#ccc',marginRight:margin+'px',fontSize: fontSize + 'px', width: fontSize + 'px', height: fontSize + 'px', lineHeight: fontSize + 'px'}">
-      <span class="vux-rater-inner">{{star}}<span class="vux-rater-outer" :style="{color: activeColor, width: cutPercent + '%'}" v-if="cutPercent > 0 && cutIndex === i-1">{{star}}</span></span>
+      <span class="vux-rater-inner"><span v-html="star"></span><span class="vux-rater-outer" :style="{color: activeColor, width: cutPercent + '%'}" v-if="cutPercent > 0 && cutIndex === i-1" v-html="star"></span></span>
     </a>
   </div>
 </template>
