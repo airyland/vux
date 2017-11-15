e02677b8c3df2ee11e91dfa510eff972b009362f
airyland <airyland@qq.com>
2017-11-15 17:42:15 +0800

    tabbar: add development tip for position usage

--- src/components/tabbar/tabbar.vue
+++ src/components/tabbar/tabbar.vue
@@ -8,6 +8,21 @@
 import { parentMixin } from '../../mixins/multi-items'
 
 export default {
+  mounted () {
+    if (process.env.NODE_ENV === 'development') {
+      this.$nextTick(() => {
+        const $el = this.$el
+        const position = window.getComputedStyle($el).position
+        if (position === 'fixed') {
+          return
+        } else if (position === 'absolute') {
+          if (document.documentElement.offsetHeight !== window.outerHeight) {
+            console.warn('[VUX warn] tabbar 定位默认为 absolute，如果你没有使用 100% 布局(view-box)，需要手动设置 style position 为 fixed')
+          }
+        }
+      })
+    }
+  },
   name: 'tabbar',
   mixins: [parentMixin],
   props: {
