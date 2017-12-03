00dfbc88c0bcb63ca77090be0d33449c0eaeecf4
airyland <airyland@qq.com>
2017-12-03 23:31:30 +0800

    panel: add slot:header slot:body #2270

--- src/components/panel/index.vue
+++ src/components/panel/index.vue
@@ -1,29 +1,32 @@
 <template>
   <div class="weui-panel weui-panel_access">
-    <div class="weui-panel__hd" v-if="header" @click="onClickHeader" v-html="header"></div>
+    <div class="weui-panel__hd" v-if="header" @click="onClickHeader" v-html="header">
+      <slot name="header"></slot>
+    </div>
     <div class="weui-panel__bd">
-      <!--type==='1'-->
-      <template v-if="type === '1'">
-        <a :href="getUrl(item.url)" v-for="item in list" @click.prevent="onItemClick(item)" class="weui-media-box weui-media-box_appmsg">
-          <div class="weui-media-box__hd" v-if="item.src">
-            <img class="weui-media-box__thumb" :src="item.src" @error="onImgError(item, $event)" alt="">
-          </div>
-          <div class="weui-media-box__bd">
+      <slot name="body">
+        <!--type==='1'-->
+        <template v-if="type === '1'">
+          <a :href="getUrl(item.url)" v-for="item in list" @click.prevent="onItemClick(item)" class="weui-media-box weui-media-box_appmsg">
+            <div class="weui-media-box__hd" v-if="item.src">
+              <img class="weui-media-box__thumb" :src="item.src" @error="onImgError(item, $event)" alt="">
+            </div>
+            <div class="weui-media-box__bd">
+              <h4 class="weui-media-box__title" v-html="item.title"></h4>
+              <p class="weui-media-box__desc" v-html="item.desc"></p>
+            </div>
+          </a>
+        </template>
+        <!--type==='2'-->
+        <template v-if="type === '2'">
+          <div class="weui-media-box weui-media-box_text" v-for="item in list" @click.prevent="onItemClick(item)">
             <h4 class="weui-media-box__title" v-html="item.title"></h4>
             <p class="weui-media-box__desc" v-html="item.desc"></p>
           </div>
-        </a>
-      </template>
-      <!--type==='2'-->
-      <template v-if="type === '2'">
-        <div class="weui-media-box weui-media-box_text" v-for="item in list" @click.prevent="onItemClick(item)">
-            <h4 class="weui-media-box__title" v-html="item.title"></h4>
-            <p class="weui-media-box__desc" v-html="item.desc"></p>
-        </div>
-      </template>
-      <!--type==='3'-->
-      <template v-if="type === '3'">
-        <div class="weui-media-box weui-media-box_small-appmsg">
+        </template>
+        <!--type==='3'-->
+        <template v-if="type === '3'">
+          <div class="weui-media-box weui-media-box_small-appmsg">
             <div class="weui-cells">
               <a class="weui-cell weui-cell_access" :href="getUrl(item.url)" v-for="item in list" @click.prevent="onItemClick(item)">
                 <div class="weui-cell__hd">
@@ -34,40 +37,41 @@
                 </div>
                 <span class="weui-cell__ft"></span>
               </a>
-            </div>
-        </div>
-      </template>
-      <!--type==='4'-->
-      <template v-if="type === '4'">
-        <div class="weui-media-box weui-media-box_text" v-for="item in list" @click.prevent="onItemClick(item)">
-          <h4 class="weui-media-box__title" v-html="item.title"></h4>
-          <p class="weui-media-box__desc" v-html="item.desc"></p>
-          <ul class="weui-media-box__info" v-if="item.meta">
-            <li class="weui-media-box__info__meta" v-html="item.meta.source"></li>
-            <li class="weui-media-box__info__meta" v-html="item.meta.date"></li>
-            <li class="weui-media-box__info__meta weui-media-box__info__meta_extra" v-html="item.meta.other"></li>
-          </ul>
-        </div>
-      </template>
-      <!--type==='5'-->
-      <template v-if="type === '5'">
-        <div class="weui-media-box weui-media-box_text" v-for="item in list" @click.prevent="onItemClick(item)">
-          <div class="weui-media-box_appmsg">
-            <div class="weui-media-box__hd" v-if="item.src">
-              <img class="weui-media-box__thumb" @error="onImgError(item, $event)" :src="item.src" alt="">
-            </div>
-            <div class="weui-media-box__bd">
-              <h4 class="weui-media-box__title" v-html="item.title"></h4>
-              <p class="weui-media-box__desc" v-html="item.desc"></p>
-            </div>
+              </div>
           </div>
-          <ul class="weui-media-box__info" v-if="item.meta">
-            <li class="weui-media-box__info__meta" v-html="item.meta.source"></li>
-            <li class="weui-media-box__info__meta" v-html="item.meta.date"></li>
-            <li class="weui-media-box__info__meta weui-media-box__info__meta_extra" v-html="item.meta.other"></li>
-          </ul>
-        </div>
-      </template>
+        </template>
+        <!--type==='4'-->
+        <template v-if="type === '4'">
+          <div class="weui-media-box weui-media-box_text" v-for="item in list" @click.prevent="onItemClick(item)">
+            <h4 class="weui-media-box__title" v-html="item.title"></h4>
+            <p class="weui-media-box__desc" v-html="item.desc"></p>
+            <ul class="weui-media-box__info" v-if="item.meta">
+              <li class="weui-media-box__info__meta" v-html="item.meta.source"></li>
+              <li class="weui-media-box__info__meta" v-html="item.meta.date"></li>
+              <li class="weui-media-box__info__meta weui-media-box__info__meta_extra" v-html="item.meta.other"></li>
+            </ul>
+          </div>
+        </template>
+        <!--type==='5'-->
+        <template v-if="type === '5'">
+          <div class="weui-media-box weui-media-box_text" v-for="item in list" @click.prevent="onItemClick(item)">
+            <div class="weui-media-box_appmsg">
+              <div class="weui-media-box__hd" v-if="item.src">
+                <img class="weui-media-box__thumb" @error="onImgError(item, $event)" :src="item.src" alt="">
+              </div>
+              <div class="weui-media-box__bd">
+                <h4 class="weui-media-box__title" v-html="item.title"></h4>
+                <p class="weui-media-box__desc" v-html="item.desc"></p>
+              </div>
+            </div>
+            <ul class="weui-media-box__info" v-if="item.meta">
+              <li class="weui-media-box__info__meta" v-html="item.meta.source"></li>
+              <li class="weui-media-box__info__meta" v-html="item.meta.date"></li>
+              <li class="weui-media-box__info__meta weui-media-box__info__meta_extra" v-html="item.meta.other"></li>
+            </ul>
+          </div>
+        </template>
+      </slot>
     </div>
     <div class="weui-panel__ft">
       <a class="weui-cell weui-cell_access weui-cell_link" :href="getUrl(footer.url)" v-if="footer && type !== '3'" @click.prevent="onClickFooter">
