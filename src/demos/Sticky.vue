dd0e8ca09b7dadd3ce63d2fe679b28e221dc1665
dongj0316 <475586651@qq.com>
2018-01-02 01:47:43 +0800

    reduction Sticky.vue demo

--- src/demos/Sticky.vue
+++ src/demos/Sticky.vue
@@ -4,33 +4,14 @@
     <br/>
     <div class="space-btn" @click="spaceChange">显示间隔</div>
     <div class="space" v-if="showSpace">间隔</div>
-    <!-- 新的用法，支持sticky用sticky，否则用fixed  -->
-    <sticky scroll-box="vux_view_box_body" ref="sticky" :offset="46">
-      <tab :line-width="1">
-        <tab-item selected>正在正映</tab-item>
-        <tab-item>即将上映</tab-item>
-      </tab>
-    </sticky>
-    <div class="vux-sticky-fill" style="height: 44px;"></div>
-
-    <!-- 新的用法，全部fixed定位 -->
-    <!-- <sticky scroll-box="vux_view_box_body" ref="sticky" :offset="46" :check-sticky-support="false">
-      <tab :line-width="1">
-        <tab-item selected>正在正映</tab-item>
-        <tab-item>即将上映</tab-item>
-      </tab>
-    </sticky>
-    <div class="vux-sticky-fill" style="height: 44px;"></div> -->
-
-    <!-- 旧的用法 -->
-    <!-- <div style="height: 44px;">
+    <div style="height:44px;">
       <sticky scroll-box="vux_view_box_body" ref="sticky" :offset="46" :check-sticky-support="false">
         <tab :line-width="1">
           <tab-item selected>正在正映</tab-item>
           <tab-item>即将上映</tab-item>
         </tab>
       </sticky>
-    </div> -->
+    </div>
     <p v-for="i in 100">{{i}}<br></p>
   </div>
 </template>
