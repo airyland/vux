21e28b4c52ed7c0c33ba1347e5f3c3b6565b0a70
airyland <airyland@qq.com>
2018-04-07 18:43:30 +0800

    popup-picker: add prop:disabled #2594

--- src/components/popup-picker/index.vue
+++ src/components/popup-picker/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="vux-cell-box">
-    <div class="weui-cell vux-tap-active weui-cell_access" @click="onClick" v-show="showCell">
+    <div class="weui-cell vux-tap-active" :class="{'weui-cell_access': !disabled}" @click="onClick" v-show="showCell">
       <div class="weui-cell__hd">
         <slot name="title" label-class="weui-label" :label-style="labelStyles" :label-title="title">
           <label class="weui-label" :class="labelClass" :style="labelStyles" v-if="title" v-html="title"></label>
@@ -141,7 +141,8 @@ export default {
     },
     columnWidth: Array,
     popupStyle: Object,
-    popupTitle: String
+    popupTitle: String,
+    disabled: Boolean
   },
   computed: {
     labelStyles () {
@@ -164,7 +165,9 @@ export default {
       return value2name(this.currentValue, this.data)
     },
     onClick () {
-      this.showValue = true
+      if (!this.disabled) {
+        this.showValue = true
+      }
     },
     onHide (type) {
       this.showValue = false
