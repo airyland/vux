06820b7ddaf32d75da87aeea74cf0d04689b97bd
airyland <airyland@qq.com>
2018-05-04 17:44:45 +0800

    lib: fix false value check #2720

--- src/libs/get-parent-prop.js
+++ src/libs/get-parent-prop.js
@@ -1,8 +1,8 @@
 export default function (self, name) {
-  if (self.$parent && self.$parent[name]) {
+  if (self.$parent && typeof self.$parent[name] !== 'undefined') {
     return self.$parent[name]
   }
-  if (self.$parent && self.$parent.$parent && self.$parent.$parent[name]) {
+  if (self.$parent && self.$parent.$parent && typeof self.$parent.$parent[name] !== 'undefined') {
     return self.$parent.$parent[name]
   }
 }
