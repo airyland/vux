c041b47fa7fd2b8f321a758560eafa392979d4eb
swiftlinlei <37229861+swiftlinlei@users.noreply.github.com>
2018-05-14 10:47:17 +0800

    日历组件初始化选定日期[(上/下)个(月/月初/月末)]
    日历组件可以初始化选定当前日期，添加初始化日期上个月、下个月、上个月初、上个月末、下个月初、下个月末。

--- src/components/calendar/index.vue
+++ src/components/calendar/index.vue
@@ -210,59 +210,59 @@ export default {
       if (!this.shouldConfirm) {
         this.show = false
       }
-    }
-  },
-  getNextMonth(date) {  
-    var arr = date.split('-');  
-    var year = arr[0]; //获取当前日期的年份  
-    var month = arr[1]; //获取当前日期的月份  
-    var day = arr[2]; //获取当前日期的日  
-    var days = new Date(year, month, 0);  
-    days = days.getDate(); //获取当前日期中的月的天数  
-    var year2 = year;  
+    },
+    getNextMonth(date) {  
+    var arr = date.split('-') 
+    var year = arr[0] //获取当前日期的年份  
+    var month = arr[1] //获取当前日期的月份  
+    var day = arr[2] //获取当前日期的日  
+    var days = new Date(year, month, 0)
+    days = days.getDate() //获取当前日期中的月的天数  
+    var year2 = year
     var month2 = parseInt(month) + 1;  
     if (month2 == 13) {  
     year2 = parseInt(year2) + 1;  
       month2 = 1;  
     }  
-      var day2 = day;  
-      var days2 = new Date(year2, month2, 0);  
-      days2 = days2.getDate();  
+      var day2 = day 
+      var days2 = new Date(year2, month2, 0) 
+      days2 = days2.getDate()
       if (day2 > days2) {  
-        day2 = days2;  
+        day2 = days2 
       }  
         if (month2 < 10) {  
-          month2 = '0' + month2;  
+          month2 = '0' + month2
         }  
           
-          var t2 = year2 + '-' + month2 + '-' + day2;  
-          return t2;  
+          var t2 = year2 + '-' + month2 + '-' + day2
+          return t2  
       }  
-  },
-  getPreMonth(date) {  
-    var arr = date.split('-');  
-    var year = arr[0]; //获取当前日期的年份  
-    var month = arr[1]; //获取当前日期的月份  
-    var day = arr[2]; //获取当前日期的日  
-    var days = new Date(year, month, 0);  
-    days = days.getDate(); //获取当前日期中月的天数  
-    var year2 = year;  
-    var month2 = parseInt(month) - 1;  
+    },
+    getPreMonth(date) {  
+    var arr = date.split('-') 
+    var year = arr[0] //获取当前日期的年份  
+    var month = arr[1] //获取当前日期的月份  
+    var day = arr[2] //获取当前日期的日  
+    var days = new Date(year, month, 0)  
+    days = days.getDate() //获取当前日期中月的天数  
+    var year2 = year 
+    var month2 = parseInt(month) - 1
     if (month2 == 0) {  
-      year2 = parseInt(year2) - 1;  
-      month2 = 12;  
+      year2 = parseInt(year2) - 1 
+      month2 = 12
     }  
-       var day2 = day;  
-       var days2 = new Date(year2, month2, 0);  
-       days2 = days2.getDate();  
+       var day2 = day  
+       var days2 = new Date(year2, month2, 0)  
+       days2 = days2.getDate()  
        if (day2 > days2) {  
-         day2 = days2;  
+         day2 = days2 
        }  
        if (month2 < 10) {  
-         month2 = '0' + month2;  
+         month2 = '0' + month2
        }  
-         var t2 = year2 + '-' + month2 + '-' + day2;  
-         return t2;  
+         var t2 = year2 + '-' + month2 + '-' + day2  
+         return t2 
+    }
   },
   watch: {
     value (newVal, oldVal) {
